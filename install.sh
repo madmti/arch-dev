1a66e7b7ded5be219eb02b599e089fc05bffcb0b
madmti <supermatrix9@gmail.com>
2025-11-28 14:25:27 -0300

    feat: add Ly display manager

--- install.sh
+++ install.sh
@@ -8,3 +8,9 @@ sudo pacman -S --needed - < pkglist.txt
 
 echo "Base instalation complete"
 
+
+echo "Configuring display manager (Ly)"
+sudo cp ~/dotfiles/etc/ly/config.ini /etc/ly/config.ini
+sudo systemctl enable ly
+
+
